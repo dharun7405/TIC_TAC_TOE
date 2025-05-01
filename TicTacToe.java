14e6085996ac3ec03882d79cc7ac8709c7b510dd
dharun69420 <dharunsr.22aid@kongu.edu>
2025-05-01 15:55:48 +0530

    Frame and TextField Added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,2 +1,49 @@
-public class TicTacToe {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class TicTacToe implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    Random random = new Random();
+    JFrame frame =new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel=new JPanel();
+    JLabel textfeild=new JLabel();
+    JButton[] buttons =new JButton[9];
+    boolean player1_turn;
+
+    TicTacToe(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700,700);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        frame.setResizable(false);
+
+        textfeild.setBackground(new Color(25,25,25));
+        textfeild.setForeground(new Color(25,255,0));
+        textfeild.setFont(new Font("Karla",Font.BOLD,75));
+        textfeild.setHorizontalAlignment(JLabel.CENTER);
+        textfeild.setText("Tic-Tac-Toe");
+        textfeild.setOpaque(true);
+
+
+    }
+    public void firstTurn(){
+
+    }
+    public void check(){
+
+    }
+    public void xWins(int a,int b,int c){
+
+    }
+    public void oWins(int a,int b,int c){
+
+    }
 }
